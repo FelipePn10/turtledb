9092f080a6e11ce789c35b11d5e4b1d47c1e1946
felipe panosso <panosso@localhost.localdomain>
2026-01-22 21:09:49 -0300

    Refactoring: The Select statement from class to Record has been updated.
    - As mentioned before, we need immutable and safe AST nodes throughout the code. Using classes made this a bit less reliable; with Record, we can have more confidence with less code.
    
    - Descriptive comments have also been implemented in the Insert grammar to show which actions are currently supported.

--- src/main/java/com/db/turtle/parser/ast/statements/SelectStatement.java
+++ src/main/java/com/db/turtle/parser/ast/statements/SelectStatement.java
@@ -5,33 +5,18 @@ import com.db.turtle.parser.ast.denominator.Expression;
 import com.db.turtle.parser.ast.ntm.TableName;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
-public final class SelectStatement implements Statement {
+public record SelectStatement(
+        List<Expression> projection,
+        TableName table,
+        Optional<Expression> where
+) implements Statement {
+    public SelectStatement {
+        Objects.requireNonNull(projection);
+        Objects.requireNonNull(table);
 
-    private final List<Expression> projection;
-    private final TableName table;
-    private final Optional<Expression> where;
-
-    public SelectStatement(
-            List<Expression> projection,
-            TableName table,
-            Optional<Expression> where
-    ) {
-        this.projection = projection;
-        this.table = table;
-        this.where = where;
-    }
-
-    public List<Expression> getProjection() {
-        return projection;
-    }
-
-    public TableName getTable() {
-        return table;
-    }
-
-    public Optional<Expression> getWhere() {
-        return where;
+        projection = List.copyOf(projection);
     }
 }
