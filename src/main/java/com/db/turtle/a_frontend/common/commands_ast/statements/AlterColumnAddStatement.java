84e01a7da5259239942f6937eac6d0b64bf1d5db
FelipePn10 <felipepanosso21@gmail.com>
2026-02-03 20:07:24 -0300

    feat: (generating an AST node for SQL ALTER commands): I implemented the  class to represent the AST node of a specific ALTER command (specifically the command to add a column to a table).
    - The class is a record that has a simple check and a basic constructor to successfully instantiate the class into something minimally usable in the future.

---
+++ src/main/java/com/db/turtle/a_frontend/common/commands_ast/statements/AlterColumnAddStatement.java
@@ -0,0 +1,36 @@
+package com.db.turtle.a_frontend.common.commands_ast.statements;
+
+import com.db.turtle.a_frontend.common.denominator.C_Statement;
+import com.db.turtle.a_frontend.impl.parser.ast.ntm.ColumnName;
+import com.db.turtle.a_frontend.impl.parser.ast.ntm.TableName;
+import com.db.turtle.a_frontend.impl.parser.ast.ntm.constraint.ColumnConstraint;
+import com.db.turtle.a_frontend.impl.parser.ast.ntm.types.DataType;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Representação de um comando ALTER ADD COLUMN em Nó do AST.
+ * */
+public record AlterColumnAddStatement (
+        TableName tableName,
+        ColumnName column,
+        DataType type,
+        List<ColumnConstraint> constraint
+) implements C_Statement {
+    public AlterColumnAddStatement(
+            TableName tableName,
+            ColumnName column,
+            DataType type,
+            List<ColumnConstraint> constraint
+    ) {
+        if (tableName == null || column == null) {
+            throw new IllegalArgumentException("Table and/or Column cannot be null");
+        }
+
+        this.tableName = tableName;
+        this.column = column;
+        this.type = type;
+        this.constraint = constraint;
+    }
+}
